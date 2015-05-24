973bbd875c7047904ef40dcfbefb9787f4b25c55
Henning Bekel <h.bekel@googlemail.com>
2015-05-24 21:26:28 +0200

    C128: Kernal: Disabled LOAD/SAVE on tape, server installed and pingable

--- kernal128.asm
+++ kernal128.asm
@@ -4,7 +4,7 @@
 
 //------------------------------------------------------------------------------	
 	
-.pc = $fa66 // wedge into system irw
+.pc = $fa66 // wedge into system irq
 wedge: {
 	jmp irq
 resume: 
@@ -12,13 +12,89 @@ eof:
 }
 
 //------------------------------------------------------------------------------
-.pc = $e8d8 // start of search tape header routine
+  
+.pc = $f6cc
+tapeIODisabledMessage: {
+.text "TAPE IO DISABLE" .byte $c4 
+eof:
+}        
+
+//------------------------------------------------------------------------------
+
+.pc = $f32f
+disableTapeLoad: {
+  ldy #27
+  jsr $f722
+  rts
+eof:    
+}        
+
+//------------------------------------------------------------------------------
+        
+.pc = $f5c8        
+disableTapeSave: {
+  ldy #27
+  jsr $f722
+  rts
+eof:    
+}
+        
+//------------------------------------------------------------------------------
+
+.pc = $e8d0 // start of search tape header routine
 irq: {
+        lda $dd0d
+	and #$10
+	beq done
+
+	ldy $dd01
+	jsr ack
+        
 done:   jsr jrsirq
         jmp wedge.resume
 eof:    
 }        
 
+//------------------------------------------------------------------------------
+	
+wait: {
+loop:   lda $dd0d
+	and #$10
+	beq loop
+	rts
+eof:	
+}
+
+//------------------------------------------------------------------------------		
+	
+ack: {
+	lda $dd00
+	eor #$04
+	sta $dd00
+	rts
+eof:
+}
+
+//------------------------------------------------------------------------------		
+
+read: {
+	jsr wait
+	ldx $dd01
+	jsr ack
+	rts
+eof:
+}
+
+//------------------------------------------------------------------------------	
+	
+write: {
+	sta $dd01
+	jsr ack
+	jsr wait
+	rts
+eof:
+}
+        
 //------------------------------------------------------------------------------		
         
 .pc = $10000
@@ -39,6 +115,14 @@ eof:
 
 .eval command = command + patch(wedge, wedge.eof)	
 .eval command = command + patch(irq, irq.eof)
+.eval command = command + patch(wait, wait.eof)
+.eval command = command + patch(ack, ack.eof)
+.eval command = command + patch(read, read.eof)                
+.eval command = command + patch(write, write.eof)
+        
+.eval command = command + patch(disableTapeLoad, disableTapeLoad.eof)
+.eval command = command + patch(disableTapeSave, disableTapeSave.eof)            
+.eval command = command + patch(tapeIODisabledMessage, tapeIODisabledMessage.eof)  
 
 .print command
 //------------------------------------------------------------------------------			
